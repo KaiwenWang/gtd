2506cd45fd35521fe04b729d2256d9826eb75c11
Charlie <root@radicaldesigns.electricembers.net>
2009-06-05 16:29:17 -0700

    fixed minor spelling error

--- view/company_table.php
+++ view/company_table.php
@@ -2,7 +2,7 @@
 
 function companyTable( $modelObjects, $o = array()){
     $out = array();
-    $out['headers'] = array('Company','Status','Balence');
+    $out['headers'] = array('Company','Status','Balance');
     $out['rows'] =  array();
     foreach($modelObjects as $m){
       $out['rows'][] = array($m->getName(),$m->getData('status'),$m->getData('balence') );
