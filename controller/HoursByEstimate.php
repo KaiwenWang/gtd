b13c7f18a3a1c8eff602ca3c204b0e8f8bf4dd6e
Charlie <root@radicaldesigns.electricembers.net>
2009-06-05 16:27:43 -0700

    added comments and message.

--- controller/HoursByEstimate.php
+++ controller/HoursByEstimate.php
@@ -5,7 +5,7 @@
     Displays a list of the estimate line items associated with a project and as sublist the logged hours associated with the Estimates.
         
     $get options array:
-    -<b>id</b> id of the project that we want to see the hours associated with it
+    -<b>project_id</b> id of the project that we want to see the hours associated with it
       
     @return html
     @package controller
@@ -22,12 +22,14 @@ class HoursByEstimate extends PageController {
     }
     function get( $get = array()){
         $r = getRenderer();
+        if (!$get['project_id']) {
+			$r->msg('bad','You need to provide a project_id ');
+			return;
+		}
 		$p = new Project($get['project_id']);
 		$estimates = $p->getEstimates();
         $html = $r->view('hoursByEstimate', $estimates, array('id'=>'test'));
-        if (!$get['project_id']) {
-			$r->msg('bad','You need to provide a project id ');
-		}
+
 		return $html;
     }
 }
