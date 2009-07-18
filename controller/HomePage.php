298b358baece373ece9a42f6b6ed90b526cd521e
ted@radicaldesigns.org <ted@gtd.radicaldesigns.org>
2009-07-18 18:40:23 +0000

    Created HomePage controller

---
+++ controller/HomePage.php
@@ -0,0 +1,18 @@
+<?php
+class HomePage extends PageController {
+    var $_class_name = 'HomePage';
+
+    function __construct(){
+        parent::__construct();
+    }
+    function get( $params = array()){
+        $r =& getRenderer();
+        return $r->template('template/standard_inside.html',
+                            array(
+                            'title'=>'All yr Defaults arr belongs to us.',
+                            'controls'=>'',
+                            'body'=>''
+                            ));
+    }        
+}
+?>
