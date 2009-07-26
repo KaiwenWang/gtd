5dc3c6dce3f4aff1cda46e3ed46da4afb1d1c31f
ted@radicaldesigns.org <ted@gtd.radicaldesigns.org>
2009-07-26 20:22:49 +0000

    working on "inline edit" style of form display

--- view/basic_list.php
+++ view/basic_list.php
@@ -14,11 +14,14 @@
 */
 function basicList( $list_items, $o){
 	$r =& getRenderer();
+
+	if( isset($o['display']) && ($o['display'] == 'inline')) $o['class'] = 'inline';
+
 	$attr = $r->attr($o);
 	$html = '';
 	if( $o['title']) $html .= '<h3 class="basic-table-header">'.$o['title'].'</h3>';
 	$html .= '<table class="basic-list" cellpadding="0" cellspacing="0">';
-	foreach( $list_items as $label=>$item) $html .= "<tr><td><label class=\"basic-list-label\">$label</label></td><td><span class=\"basic-list-item\">$item</span></td></tr>";
+	foreach( $list_items as $label=>$item) $html .= "<tr class=\"".$class."\"><td class=\"label-cell\"><label class=\"basic-list-label\">$label</label></td><td class=\"data-cell\"><span class=\"basic-list-item\">$item</span></td></tr>";
 	$html .= '</table>';
 	return "<div $attr >$html</div>";
 }
--- view/hour_edit_form.php
+++ view/hour_edit_form.php
@@ -8,15 +8,18 @@ function hourEditForm( $h, $o = array()){
     
     $list_items = array(
 		'ID'			=> $h->id,
-        'Date Completed'=> $r->field( $h, 'date'),
     	'Estimate' 		=> $estimate_field,
        	'Description' 	=> $r->field( $h, 'description'),
+        'Date Completed'=> $r->field( $h, 'date'),
         'Staff' 		=> $r->field( $h, 'staff_id'),
         'Hours' 		=> $r->field( $h, 'hours'),        
         'Discount' 		=> $r->field( $h, 'discount'),
 		'Basecamp ID' 	=> $r->field( $h, 'basecamp_id')
     );
-    $form_contents = $r->view( 'basicList', $list_items, array('title'=>'Edit Hour "'.$h->getName().'"')) . $r->submit();
+    $form_contents = $r->view( 'basicList', 
+    							$list_items, 
+    							array( 'title'=>'Edit Hour "'.$h->getName().'"', 'display'=>'inline')
+    							).$r->submit();
     
     return $r->form( 'post', 'HourEdit', $form_contents, array( 'redirect' => $r->url('HourEdit',$h)));
 }
