c7bfc3b51126b4bd7eeb3f996d244f8d12753d32
ted@radicaldesigns.org <ted@radicaldesigns.org>
2009-08-22 10:10:12 +0000

    a litte naming cleanup

--- controller/PageController.php
+++ controller/PageController.php
@@ -13,14 +13,14 @@ class PageController {
     function execute( $action, $params = array()){
         $this->params = $params;
     	if ( method_exists( $this, $action)){
-			$beforeAction = 'before'.ucfirst( $action);
+				$beforeAction = 'before'.ucfirst( $action);
 	   		$afterAction = 'after'.ucfirst( $action);
 	    	if ( method_exists( $this, $beforeAction)) $this->$beforeAction();
-            $this->current_action = $action;
+	    	$this->current_action = $action;
     		$this->response = $this->$action( $this->params);
 			if ( method_exists( $this, $afterAction)) $this->$afterAction();
 			if ( $this->response) return $this->response;
-			bail( 'No response from this action.');
+				bail( 'No response from this action.');
     	} else {
     		bail( 'invalid or blank action requested: '.$action);
     	}
@@ -30,14 +30,14 @@ class PageController {
     }
     protected function beforePost( ){
     	$record_set = $this->params['ActiveRecord'];
-		foreach($record_set as $class_name => $object_set){
-			foreach( $object_set as $id => $updated_fields){
-				if( $id == 'new') bail('Can\'t create new objects yet');
-				$obj = new $class_name( $id);
-				$obj->mergeData( $updated_fields);
-				$this->posted_objects[]=$obj;
+			foreach($record_set as $class_name => $object_set){
+				foreach( $object_set as $id => $updated_fields){
+					if( $id == 'new') bail('Can\'t create new objects yet');
+					$obj = new $class_name( $id);
+					$obj->mergeData( $updated_fields);
+					$this->posted_objects[]=$obj;
+				}
 			}
-		}
     }
     function post(){
 		// MUST be defined in a subclass
@@ -47,11 +47,11 @@ class PageController {
         $this->method = $method;
     }
     protected function afterPost( ){
-		foreach( $this->posted_objects as $obj) $obj->save();
-		if( $this->params('redirect')) {
-			header('Location: '.$this->params('redirect').'');
-			die();
-		}
+			foreach( $this->posted_objects as $obj) $obj->save();
+			if( $this->params('redirect')) {
+				header('Location: '.$this->params('redirect').'');
+				die();
+			}
     }
     protected function params( $key ){
         if( isset( $this->params[$key]) && $this->params[$key]) {
@@ -59,27 +59,19 @@ class PageController {
         }
         return false;
     }
-    function layout_for_action( ){
+    function template_path_for( $action){
         return 'template/standard_inside.html';
     }
-
     function display( $params, $o){
         $r = getRenderer( );
         $params['r'] = $r;
+        
         isset( $o['action'])? $action = $o['action'] 
                             : $action = $this->current_action;
 
         $view_name = strtolower( str_replace( "Controller", "", get_class( $this ))) . ucwords( $action );
 
-
-        return $r->template( $this->layout_for_action( $action), $r->view( $view_name, $params ));
-        /*
-                array(
-                    'title' 	=> $title,
-                    'controls'	=> $controls,
-                    'body' 		=> $info.$form.$hour_table.$estimate_table
-                    ));
-         */
+        return $r->template( $this->template_path_for( $action), $r->view( $view_name, $params ));
     }
 }
 ?>
