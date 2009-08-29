4268d76f0597b8d10ee82e01384f024056324c98
ted@radicaldesigns.org <ted@radicaldesigns.org>
2009-08-29 03:25:11 +0000

    hacking away on filters

--- controller/PageController.php
+++ controller/PageController.php
@@ -1,42 +1,50 @@
 <?php
-class PageController {
-    var $authentication_level = array( 'staff');
-    var $_class_name = 'PageController';
-    var $method;
+class PageController extends Collection {
     var $current_action;
     var $params = array();
-    var $posted_records = array();
+	var $data;
     var $response;
-    var $data;
+
     var $display_options = array();
- 	var $before_actions = array( 'get_posted_records' => array('create','update','destroy') );
-	var $after_actions = array( 'save_posted_records' => array('create','update','destroy') );
-    
+
+    var $filter_sequences = array();
+        
+    var $filter_collection_name = 'defaultFilterCollection';
+ 	var $before_filters = array( 'get_posted_records' => array('create','update','destroy') );
+	var $after_filters = array( 'save_posted_records' => array('create','update','destroy') );
+    var $around_filters = array();
+
     function __construct(){
         $this->data = new Collection();
+        $this->loadFilterCollection();
+        $this->generateFilterSequences();
     } 
     function execute( $action, $params = array()){
         $this->params = $params;
     	$this->current_action = $action;
-    	$this->executeActions();
-		$this->response = $this->display();
+    	
+    	$this->executeActionChain();
+    	
+		$this->response = $this->renderResponse();
     	if ( $this->response) return $this->response;
-    	bail( 'This action was valid, but did not render any html.');
+     	bail( 'This action was valid, but did not render any html, and was not set to redirect to another action');
     }
-    function executeActions( ){
+	function params( $key ){
+        if( isset( $this->params[$key]) && $this->params[$key]) {
+            return $this->params[$key];
+        }
+        return false;
+    }    
+    function executeActionChain( ){
     	if ( !method_exists( $this, $this->current_action)) bail( 'non-existent action requested: '.$action);
-   		$this->executeBeforeActions();
+    	
+  		$this->executeFilterSequence('around_filters');
+   		$this->executeFilterSequence('before_filters');
 		$this->{$this->current_action}( $this->params);
-		$this->executeAfterActions();
+   		$this->executeFilterSequence('after_filters');
+  		$this->executeFilterSequence('around_filters');
+
 		$this->nextAction();
-    }
-    function executeBeforeActions(){
-    	$beforeAction = 'before'.ucfirst( $action);
-    	if ( method_exists( $this, $beforeAction)) $this->$beforeAction();
-    }
-    function executeAfterActions(){
-    	$afterAction = 'after'.ucfirst( $action);
-    	if ( method_exists( $this, $afterAction)) $this->$afterAction();
     }
     function nextAction(){
     	if(!$this->redirect_options) return;
@@ -44,40 +52,7 @@ class PageController {
     	$this->redirect_options = array();
     	$this->executeActions();
     }
-    function redirectTo( $o = array()){
-    	$this->redirect_options = $o;
-    }    
-    protected function beforePost( ){
-    	$record_set = $this->params['ActiveRecord'];
-			foreach($record_set as $class_name => $object_set){
-				foreach( $object_set as $id => $updated_fields){
-					if( $id == 'new') bail('Can\'t create new objects yet');
-					$obj = new $class_name( $id);
-					$obj->mergeData( $updated_fields);
-					$this->posted_records[]=$obj;
-				}
-			}
-    }
-    function setMethod( $method ){
-        $this->method = $method;
-    }
-    protected function afterPost( ){
-			foreach( $this->posted_records as $obj) $obj->save();
-			if( $this->params('redirect')) {
-				header('Location: '.$this->params('redirect').'');
-				die();
-			}
-    }
-    protected function params( $key ){
-        if( isset( $this->params[$key]) && $this->params[$key]) {
-            return $this->params[$key];
-        }
-        return false;
-    }
-    function template_path_for( $action){
-        return 'template/standard_inside.html';
-    }
-    function display(){
+    function renderResponse(){
         $r = getRenderer( );
         
         isset( $this->display_options['action'])? $action = $this->display_options['action'] 
@@ -86,6 +61,36 @@ class PageController {
         $view_name = strtolower( str_replace( "Controller", "", get_class( $this ))) . ucwords( $action );
 
         return $r->template( $this->template_path_for( $action), $r->view( $view_name, $this->data ));
+    }    
+    function redirectTo( $o = array()){
+    	$this->redirect_options = $o;
+    }    
+    private function template_path_for( $action){
+        return 'template/standard_inside.html';
+    }
+    protected function executeFilterSequence( $filter_sequence_name){
+    	$filter_sequence = $this->filter_sequences_for[$this->current_action][$filter_sequence_name];
+    	foreach ($filter_sequence as $filter){
+    		method_exists( $this->filterCollection, $filter)	? $this->filterCollection->$filter()
+    										: bail("Filter <b>$filter</b> does not exist");
+    	}
+    	
+    };    
+    private function generateFilterSequences(){
+    	$this->filter_sequences_for = array();
+    	$filter_sequences = array('before_filters','after_filters','around_filters');
+    	foreach( $filter_sequences as $filter_sequence_name){}
+	    	foreach( $filter_sequence_name as $filter => $action_set){
+				foreach( $action_set as $action ){
+					if( array_key_exists($this->current_action, $action_set) ) $this->filter_sequences_for[$action][$filter_sequence_name][] = $filter;
+				}
+	    	}
+    	}
+    }
+    private function LoadFilterCollection(){
+		require_once('controller/filter/'.$this->filter_collection_name);
+    	$this->filter_collection = new $this->filter_collection_name;
+    	$this->filter_collection->controller = $this;
     }
 }
 ?>
---
+++ controller/filter/defaultFilterCollection.php
@@ -0,0 +1,25 @@
+<?php
+class defaultFilterCollection {
+	var controller;
+    protected function get_posted_records( ){
+    	$record_set = $this->controller->params['ActiveRecord'];
+			foreach( $record_set as $class_name => $object_set){
+				foreach( $object_set as $id => $updated_fields){
+					if( $id == 'new') bail('Can\'t create new objects yet');
+					$obj = new $class_name( $id);
+					$obj->mergeData( $updated_fields);
+					
+					$this->controller->posted_records[]=$obj;
+				}
+			}
+    }
+
+    protected function save_posted_records( ){
+			foreach( $this->controller->posted_records as $obj) $obj->save();
+			if( $this->controller->params('redirect')) {
+				header('Location: '.$this->params('redirect').'');
+				die();
+			}
+    }
+}
+?>
