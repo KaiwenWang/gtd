9580d6221cca0ebd592a7bb245eda232a31bdd3b
tedsuo <ted@radicaldesigns.org>
2010-03-28 11:44:47 -0700

    commented out timer

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
 require_once('utility/Timer.php');
-Timer::start('Page Request');
+//Timer::start('Page Request');
 
 require_once('boot/bootstrap.php');
 $f = new FrontController();
 echo $f->execute();
 
-Timer::stop();
+//Timer::stop();
