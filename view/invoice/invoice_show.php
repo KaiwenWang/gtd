8f0e9f8008db73eecb6f9ccec5ed7583da19de99
margot <margot@radicaldesigns.org>
2010-04-30 03:10:17 +0000

    arrg git evil sometimes - updating invoices

--- view/invoice/invoice_show.php
+++ view/invoice/invoice_show.php
@@ -2,25 +2,31 @@
 
 function invoiceShow($d, $o = array() ) {
     $r = getRenderer();
-    $project_hours = $d->company->getProjectHours();
-    $project_charges = $d->company->calculateProjectCharges($project_hours);
-    $support_hours = $d->company->getSupportHours();
-    $support_charges = $d->company->calculateSupportCharges($support_hours);
-    $total_addons = $d->company->getChargesTotal(); 
-    $payments_to_date = $d->company->getPaymentsTotal();
-    // and here we calculate all the shit we calculated above, YET AGAIN. 
-    // pretty inefficient.
-    $total = "$ " . number_format($d->invoice->getAmount(), 2);
+    $total = $d->invoice->getAmountDue();
+
+    $invoice_period = $d->invoice->getStartDate() 
+			        	. " through " 
+        				. $d->invoice->getEndDate();
+	$banner = array(
+		'Invoice Date'      => $d->invoice->getEndDate(),
+		'Invoice Number'      => "#" . $d->invoice->getData('id')
+		);
     $items = array(
-        'Project charges' => $project_charges,
-        'Support charges' => $support_charges,
-        'Addon charges' => $total_addons,
-        'Total Payments' => $payments_to_date,
-        'Total Due' => $total
+        ''            => $d->company->getName(),
+        'Items for Period'  => $invoice_period,
+        'Previous Balance'  => "$ " . number_format( $d->invoice->getPreviousBalance(), 2), 
+        'New Charges'       => "$ " . number_format( $d->invoice->getNewCosts(), 2 ),
+        'New Payments'       => "$ " . number_format( $d->invoice->getNewPaymentsTotal(), 2 ),
+        'Total Due'         => "$ " . number_format( $d->invoice->getAmountDue(), 2)
         );
-    $invoice_items = $r->view('chargeTable', $d->charges, $o )
-                    .$r->view('hourTable', $d->project_hours, $o)
-                    .$r->view('supporthourTable', $d->support_hours, $o);
+
+    $summary = '
+				<div id="banner">'.$r->view('basicList', $banner).'</div>
+                <div id="invoice-summary">
+                    '.$r->view('basicList', $items ).'
+                </div>
+                ';
+
     return array( 
         'title' => 'Show Invoice', 
         'body' => $r->view('basicList', $items )
