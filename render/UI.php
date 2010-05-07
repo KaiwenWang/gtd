5fde5a502b639a904c0ee11aeb02348d92131f29
Radical Designs <dev@radicaldesigns.org>
2010-05-07 15:35:47 -0700

    Added comments to the link function

--- render/UI.php
+++ render/UI.php
@@ -5,7 +5,15 @@ class UI {
 		$b = new Button($params);
 		return $b->execute();
 	}
-	static function link($params,$o){
+	// array $params takes the following items:
+	// text: link text to display
+	// controller: controller name (same case as the controller class)
+	// action: controller action
+	//
+	// any other params passed in go into the uri string and are passed to the action as params 
+	//
+	// alternatively, pass the param 'url' to hard-code a url.
+	static function link($params,$o = array()){
 		$l = new Link($params,$o);
 		return $l->execute();
 	}
