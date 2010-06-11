20777f68cc773f59bb2bcda0cff5f24cd1914f75
margot <margot@radicaldesigns.org>
2010-06-11 22:19:50 +0000

    added checkbox to the table

--- view/invoice/invoice_table.php
+++ view/invoice/invoice_table.php
@@ -5,10 +5,12 @@ function invoiceTable( $invoices, $options = array( )) {
     $table = array();
     $table['headers'] = array(	
 								'ID',
+								'Add',
 								'Client',
     							'Start Date',
                                 'End Date',
                                 'Batch',
+								'Send',
     							'Sent Date',
     							'Amount',
 								'Edit',
@@ -32,12 +34,19 @@ function invoiceTable( $invoices, $options = array( )) {
 											'action'=>'destroy',
 											'id'=>$i->id
 											));
+		$email_button= UI::button( array(	'controller'=>'Invoice',
+											'action'=>'email',
+											'id'=>$i->id
+											));
+      	
       	$table['rows'][] = array(	
 								$i->id,
+								"<input type=\"checkbox\" id=\"invoice-email-".$i->id."\" name=\"invoice-email-".$i->id."\">",
 								$r->link('Company', array('action' => 'show', 'id' => $c->id), $c->getName()),
       							$r->link( 'Invoice', array('action' => 'show', 'id' => $i->id ), $i->getData( 'start_date')),
       							$i->getData('end_date'),
 								$batch_link,
+								$email_button,
       							"<a href='$url' target='_blank'>" . $i->getData('sent_date') . "</a>",
       							$i->getAmountDue(),
 								$edit_button,
