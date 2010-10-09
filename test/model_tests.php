efb33140f69a9fbafd90c0ae7f1cbb9ef3c91951
Micah Lee & Ted Young <dev+micahflee+tedsuo@radicaldesigns.org>
2010-10-08 17:44:45 -0700

    fixed horrible bug from hell

--- test/model_tests.php
+++ test/model_tests.php
@@ -1,15 +1,16 @@
 <?php
-class testAmpOrm extends UnitTestCase {
-	function testCloneRecord(){
-        $id = 20973;
-        $old_hour = new Hour( $id);
-        $this->assertEqual( $old_hour->getHours( ), 99.5);
-		$new_hour = $old_hour->cloneRecord();
-        $this->assertEqual( $new_hour->getHours( ), 99.5);
-        $this->assertFalse( $new_hour->id );
-	}
-}
 class testActiveRecord extends UnitTestCase{
+	function testSave(){
+		$c = new Company();
+		$this->assertFalse($c->id, 'should not have an id yet');
+		$c->set(array('name'=>'save_test'));
+		$c->save();
+		$this->assertTrue($c->id, 'should have an id');
+
+		$this->assertEqual($c->get('name'),'save_test', 'name should be save_test');
+		$c2 = new Company($c->id);
+		$this->assertEqual($c2->get('name'),'save_test', ' reloaded item name should also be save_test');
+	}
 	function testDestroyAssociatedRecords(){
 		$c = new Company();
 		$c->set(array('name'=>'destroy_test'));
@@ -67,104 +68,3 @@ class testActiveRecord extends UnitTestCase{
 		}
 	}
 }
-class testHour extends UnitTestCase {
-	function testGetters( ) {
-        $id = 20973;
-        $h = new Hour( $id);
-        $this->assertEqual( $h->getHours( ), 99.5);
-        $this->assertEqual( $h->getDiscount( ), 96.5);
-	}
-}
-class testEstimate extends UnitTestCase {
-	function testGetters( ) {
-        $id = 20972;
-        $e = new Estimate( $id);
-		$hours = $e->getHours();
-		$this->assertIsA($hours,'array');
-		$this->assertEqual(count($hours),2);
-		foreach ($hours as $h){
-			$this->assertIsA($h, 'Hour');
-		}
-		$this->assertTrue($e->getTotalHours() == 102); 
-		$this->assertTrue($e->getBillableHours() == 4.5);
-		$this->assertTrue($e->getLowEstimate() == 95);
-		$this->assertTrue($e->getHighEstimate() == 98);
-	}
-}
-class testProject extends UnitTestCase {
-	function setUp(){
-        $id = 20971;
-        $this->project = new Project(  $id);	
-	}
-	function testGetEstimates( ) {
-
-		$estimates= $this->project->getEstimates();
-		$this->assertIsA($estimates,'array');
-		$this->assertEqual(count($estimates),2);
-		foreach ($estimates as $e){
-			$this->assertIsA($e, 'Estimate');
-		}
-	}
-	function testGetHours(){	
-		$hours = $this->project->getHours();
-		$this->assertEqual(count($hours),3);
-		$this->assertIsA($hours,'array');
-		foreach ($hours as $h){
-			$this->assertIsA($h, 'Hour');
-		}
-	}
-	function testFinder(){
-		$finder = new Project();
-		$projects = $finder->find( array('company_id'=> '20970'));
-		$this->assertIsA($projects,'array');
-		$this->assertEqual(count($projects),1);		
-	}
-	function testGetCalculations(){
-		$this->assertEqual($this->project->getTotalHours(), 107); 
-		$this->assertEqual($this->project->getBillableHours(), 8.5);
-		$this->assertEqual($this->project->getLowEstimate(), 97.5);
-		$this->assertEqual($this->project->getHighEstimate(), 101.5);
-	}
-}
-class testSupportContract extends UnitTestCase {
-	function setUp( ) {
-        $id = 20978;
-        $this->contract = new SupportContract( $id);
-	}
-	function testGetHours(){
-		$hours = $this->contract->getHours();
-		$this->assertEqual(count($hours),2);
-		$this->assertIsA($hours,'array');
-		foreach ($hours as $h){
-			$this->assertIsA($h, 'Hour');
-		}
-		$this->assertEqual($this->contract->getTotalHours(), 5.5); 
-		$this->assertEqual($this->contract->getBillableHours(), 3.5);
-	}
-}
-
-class testCompany extends UnitTestCase {
-	function setUp( ) {
-        $id = 20970;
-        $this->company = new Company( $id);
-	}
-	function testGetProjects(){
-		$this->assertEqual($this->company->getName(),'SimpleTest Company');
-		$projects= $this->company->getProjects();
-		$this->assertIsA($projects,'array');
-		$this->assertEqual(count($projects),1);
-		foreach ($projects as $p){
-			$this->assertIsA($p, 'Project');
-		}
-	}
-	function testGetSupportContracts(){
-		$support_contracts= $this->company->getSupportContracts();
-		$this->assertIsA($support_contracts,'array');
-		$this->assertEqual(count($support_contracts),1);
-		foreach ($support_contracts as $s){
-			$this->assertIsA($s, 'SupportContract');
-		}
-	}
-}
-
-?>
