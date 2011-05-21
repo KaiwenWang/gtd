bf8d7aeaecf75f99e9b3625525afe7b78af3d0f9
margot <margot@radicaldesigns.org>
2011-05-21 20:27:55 +0000

    fixing the session bug in bookmarks

--- view/application/application_layout.php
+++ view/application/application_layout.php
@@ -11,10 +11,10 @@ function applicationLayout(){
 	$form->content = $f;
 	$client_search_form = $form->html;
 	$bookmark_widget = $r->view('bookmarkWidget', array());
-
-	$bookmarks = Session::getUser()->getBookmarks();
-  $bookmark_list = $r->view('bookmarkTable', $bookmarks);
-
+ 	if ( Session::sessionExists()) {
+			$bookmarks = Session::getUser()->getBookmarks();
+			$bookmark_list = $r->view('bookmarkTable', $bookmarks);
+			}
 	return array(
 		'client_quicksearch'=> $client_search_form,
 		'bookmark' => $bookmark_widget,
