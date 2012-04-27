83f4005b7910005d530b17d17063b4f93407c635
Radical Designs <dev@radicaldesigns.org>
2012-04-27 15:57:45 -0700

    no headers on support hour table since it's not searchable

--- view/supporthour/supporthour_table.php
+++ view/supporthour/supporthour_table.php
@@ -34,10 +34,6 @@ function supporthourTable( $hours, $o = array()){
       							);
     }
     if ( !isset($o['title'])) $o['title'] = 'Hours';
-    $html = 
-         '<div class="totals-data totals-hours"><h3 class="basic-table-header">Total Support Hours: ' . $total_hours . '</h3></div>'
-        . '<div class="totals-data totals-hours"><h3 class="basic-table-header">Billable Support Hours: ' . $billable_hours . "</h3></div>"
-        . $r->view( 'basicTable', $table,
-          array('title'=>'Support Hours', $o)) ;
+    $html = $r->view( 'basicTable', $table, array('title'=>'Support Hours', $o)) ;
     return $html;
 }
