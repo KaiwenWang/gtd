ab2726042597c7d4b2a4200c0c71a21dfe6569c6
Radical Designs <dev@radicaldesigns.org>
2012-04-27 14:27:41 -0700

    Whitespace

--- controller/CompanyController.php
+++ controller/CompanyController.php
@@ -1,97 +1,107 @@
 <?php
 class CompanyController extends PageController {
-	public $template = 'gtd_main_template';
- 	var $before_filters = array( 'get_posted_records' => array( 'create','update','destroy'),
-								 'get_search_criteria'=> array('index','kneecaps')
- 								);
-    function index( $params){
-		$criteria = array();
-		if( !empty($this->search_for_companies)) $criteria = $this->search_for_companies;
-		$criteria['sort'] = 'status, name';
+  public $template = 'gtd_main_template';
+  var $before_filters = array('get_posted_records' => array( 'create','update','destroy'),
+                              'get_search_criteria'=> array('index','kneecaps'));
 
-        $this->data->companies = Company::getMany($criteria);
+  function index( $params){
+    $criteria = array();
+    if( !empty($this->search_for_companies)) $criteria = $this->search_for_companies;
+    $criteria['sort'] = 'status, name';
 
-        $this->data->new_company = new Company();
+    $this->data->companies = Company::getMany($criteria);
+    $this->data->new_company = new Company();
+    $this->data->search_company = new Company();
+    $this->data->search_company->set($criteria); 
+  } 
 
-		$this->data->search_company = new Company();
-        $this->data->search_company->set($criteria); 
-        
-    }        
-	function show($params){
-		$params['id']	? $this->data->company = new Company( $params['id'])
-						: bail('no company selected');
+  function show($params){
+    if(empty($params['id']) bail('no company selected');
 
-		$user_id = Session::getUserId();
+    $this->data->company = new Company( $params['id']);
+    $user_id = Session::getUserId();
     
     $p = new Project();	
-		$p->set( array(	'company_id'=>$params['id'],
-						'staff_id'=>$user_id
-					  ));
-		$this->data->new_project = $p;
-		
-		
-		$this->data->new_note = new Note();
-		$this->data->new_note->set(array( 
-										'date' => date('Y-m-d'),
-										'staff_id'=>Session::getUserId(),
-									 	'company_id' => $params['id'] ));
+    $p->set(array(
+      'company_id'=>$params['id'],
+      'staff_id'=>$user_id
+    ));
+    $this->data->new_project = $p;
+	
+    $this->data->new_note = new Note();
+    $this->data->new_note->set(array( 
+      'date' => date('Y-m-d'),
+      'staff_id'=>Session::getUserId(),
+      'company_id' => $params['id'] 
+    ));
 
-		$this->data->new_charge = new Charge();
-		$this->data->new_charge->set(array( 
-										'date' => date('Y-m-d'),
-									  	'company_id' => $params['id'] ));
+    $this->data->new_charge = new Charge();
+    $this->data->new_charge->set(array( 
+      'date' => date('Y-m-d'),
+      'company_id' => $params['id'] 
+    ));
 
-		$this->data->new_payment = new Payment();
-		$this->data->new_payment->set(array( 
-										'date' => date('Y-m-d'),
-									  	'company_id' => $params['id'] ));
+    $this->data->new_payment = new Payment();
+    $this->data->new_payment->set(array( 
+      'date' => date('Y-m-d'),
+      'company_id' => $params['id'] 
+    ));
 
-		$this->data->new_invoice = new Invoice();
-		$this->data->new_invoice->set(array( 
-									  	'company_id' => $params['id'] ));
+    $this->data->new_invoice = new Invoice();
+    $this->data->new_invoice->set(array( 
+      'company_id' => $params['id'] 
+    ));
 
-		$this->data->new_contact = new Contact();
-		$this->data->new_contact->set(array( 
-									  	'company_id' => $params['id'] ));
-  	}
+    $this->data->new_contact = new Contact();
+    $this->data->new_contact->set(array( 
+      'company_id' => $params['id'] 
+    ));
+  }
 
-	function create( $params){
-		$company = $this->new_companies[0];
-    	$company->save();
+  function create( $params){
+    $company = $this->new_companies[0];
+    $company->save();
 
-		if(!empty($this->new_contacts)){
-			$contact = $this->new_contacts[0];
-			$contact->set(array('company_id'=>$company->id));
-	    	$contact->save();
-		}
-    	$this->redirectTo( array('controller'=>'Company',
-    							 'action' => 'show',
-    							 'id'=>$company->id
-    							 ));
+    if(!empty($this->new_contacts)){
+      $contact = $this->new_contacts[0];
+      $contact->set(array('company_id'=>$company->id));
+      $contact->save();
     }
-	function update( $params){
-		$c = $this->updated_companies[0];
-    	$c->save();
+    $this->redirectTo( array(
+      'controller'=>'Company',
+      'action' => 'show',
+      'id'=>$company->id
+    ));
+  }
 
-   		Render::msg($c->getName().' Updated.');	
+  function update( $params){
+    $c = $this->updated_companies[0];
+    $c->save();
 
-	 	$this->redirectTo( array('controller'=>'Company',
-    							 'action' => 'show',
-    							 'id'=>$c->id
-    							 ));
-    }
-    function new_form( $params){
-		$this->data = new Company();
-    }
-	function kneecaps( $params){
-		$criteria = array();
-		if( !empty($this->search_for_companies)) $criteria = $this->search_for_companies;
-		$criteria['sort'] = 'status, name';
-        $this->data->companies = Company::getMany($criteria);
+    Render::msg($c->getName().' Updated.');	
 
-		$this->data->search_company = new Company();
-        $this->data->search_company->set($criteria); 
-    }
-    function destroy(){
-    }
+    $this->redirectTo( array(
+      'controller'=>'Company',
+      'action' => 'show',
+      'id'=>$c->id
+    ));
+  }
+
+  function new_form( $params){
+    $this->data = new Company();
+  }
+
+  function kneecaps( $params){
+    $criteria = array();
+    if( !empty($this->search_for_companies)) $criteria = $this->search_for_companies;
+    $criteria['sort'] = 'status, name';
+    $this->data->companies = Company::getMany($criteria);
+
+    $this->data->search_company = new Company();
+    $this->data->search_company->set($criteria); 
+  }
+
+  function destroy(){
+  }
+
 }
