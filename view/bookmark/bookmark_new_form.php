865ec6d02ab3752014adb20ff05e468dbb71fb8a
Tesla Dethray <TeslaDethray@gmail.com>
2013-05-24 13:09:58 -0700

    Fixed issue #91. When editing the look of the new-bookmark form, a line was commented out which caused the whole problem.

--- view/bookmark/bookmark_new_form.php
+++ view/bookmark/bookmark_new_form.php
@@ -5,7 +5,7 @@ function bookmarkNewForm($d, $o = array()){
   if(isset($url['query']) && $url['query'] != '') {
     $local_url .= '?' . $url['query'];
   }
-    
+  
   $r =& getRenderer();
   $form = new Form( array( 'controller'=>'Bookmark', 'action'=>'create'));
   $fs = $form->getFieldSetFor($d->bookmark);
@@ -13,7 +13,7 @@ function bookmarkNewForm($d, $o = array()){
   //$html = '<h3 class="basic-form-header">Bookmark This Page</h3>'; 
   $html .= $fs->field('staff_id',array('field_type'=>'hidden'));
   //$html .= $fs->field('source',array('field_type'=>'hidden', 'value' => $local_url));
-  //$html .= '<input type="hidden" value="' . $local_url . '" id="ActiveRecord[Bookmark][new-0][source]" class="source-field Bookmark-field hidden-field" name="ActiveRecord[Bookmark][new-0][source]">';
+  $html .= '<input type="hidden" value="' . $local_url . '" id="ActiveRecord[Bookmark][new-0][source]" class="source-field Bookmark-field hidden-field" name="ActiveRecord[Bookmark][new-0][source]">';
   $html .= $fs->description;
   $html .= $form->getSubmitBtn();
   $form->content = $html; 
