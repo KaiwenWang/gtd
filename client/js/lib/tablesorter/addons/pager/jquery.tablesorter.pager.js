49de5103160bef4a05455603238e840e527d7659
pro on gtd <dev@radicaldesigns.org>
2013-08-16 18:42:28 -0700

    OK, fixed it for real this time. (dammit)

--- client/js/lib/tablesorter/addons/pager/jquery.tablesorter.pager.js
+++ client/js/lib/tablesorter/addons/pager/jquery.tablesorter.pager.js
@@ -19,12 +19,6 @@
                                 var c = table.config;
                                 if(!c.pagerPositionSet && c.positionFixed) {
                                         var c = table.config, o = $(table);
-                                        if(o.offset) {
-                                                c.container.css({
-                                                        top: o.offset().top + o.height() + 'px',
-                                                        position: 'absolute'
-                                                });
-                                        }
                                         c.pagerPositionSet = true;
                                 }
                         }
